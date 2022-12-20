50dfd5918b62c685c9b06bc24c4b039643942a58
Yahel Bareket <yahelbareket@gmail.com>
2022-12-20 22:48:27 +0200

    Added capital letters case

--- Server/Input Receiver/Input Receiver/KeyboardMessage.cpp
+++ Server/Input Receiver/Input Receiver/KeyboardMessage.cpp
@@ -45,12 +45,12 @@ void KeyboardMessage::updateServer()
 
 			// setting up a generic keyboard event.
 			ip.type = INPUT_KEYBOARD;
-			ip.ki.wScan = 0; // hardware scan code for key
+			ip.ki.wScan = (this->_key[0] < 65 || this->_key[0] > 90) ? 0 : this->_key[0]; // hardware scan code for key
 			ip.ki.time = 0;
 			ip.ki.dwExtraInfo = 0;
 
-			ip.ki.wVk = this->_key[0];  // inserting the key's value to be pressed
-			ip.ki.dwFlags = 0; // 0 for key press
+			ip.ki.wVk = (this->_key[0] < 65 || this->_key[0] > 90) ? VkKeyScan(this->_key[0]) : 0; // inserting the key's value to be pressed
+			ip.ki.dwFlags = KEYEVENTF_UNICODE; // 0 for key press
 			SendInput(1, &ip, sizeof(INPUT));
 		}
 	}
