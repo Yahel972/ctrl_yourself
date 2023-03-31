cba26b13a287c3dc532c19fb07104ac5d2eaf9f8
Yahel972 <yahelbareket@gmail.com>
2023-03-31 23:05:52 +0300

    Added login page

---
+++ Client/Client/LoginForm.cpp
@@ -0,0 +1,14 @@
+﻿#include "LoginForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+int main()
+//int main (array＜String^＞^args )
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Client::LoginForm form;
+	Application::Run(% form);
+	return 0;
+}
